640b7c89ccc768151d0f7e5bd4c8abbace51f2ff
SarahRodrigues20 <113391554+SarahRodrigues20@users.noreply.github.com>
2022-09-30 08:42:19 -0300

    Add files via upload

---
+++ atividade01.py
@@ -0,0 +1,18 @@
+nota1:float; nota2:float; nota3:float;
+
+nota1=float(input("Digite sua nota em Trabalho de Laborátorio: "))
+nota1= nota1 * 2
+
+nota2=float(input("Digite sua nota em Avaliação Semestral: "))
+nota2= nota2 *3
+
+nota3=float(input("Digite sua nota em Exame Final: "))
+nota3= nota3 *5
+
+speso= nota1+nota2+nota3
+snota= 2+3+5
+media=snota/speso
+
+if media >= 9:
+   print("sua nota é: {round,2(media)}") S
+
